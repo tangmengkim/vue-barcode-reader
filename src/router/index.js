0000ff96fe5077cd6e4a97f5a3c4caa298b6d831
mengkim168 <mengkimtang@gmail.com>
2024-10-28 11:03:43 +0700

    update

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import HomePage from '@/views/HomePage.vue'
 import ScanPage from '@/views/ScanPage.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
